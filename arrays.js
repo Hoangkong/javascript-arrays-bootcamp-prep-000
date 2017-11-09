49645aaaed1bf2990d7dbd3efadd50f1b0a2d9d3
Michael Hoang <michael_hoang@outlook.com>
2017-11-09 19:00:07 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kit kat", "skittles"];
+
+addElementToBeginningOfArray(array, element) {
+  [element, ...array]
+  return newarray = [element,...array]
+}
